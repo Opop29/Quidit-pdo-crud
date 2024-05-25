42e76a33b821af011fbbf1eb745941ffaea542f9
joshua <joshua@gmai.com>
2024-05-25 23:16:35 +0800

    add cancel button on address php

--- backups/address.php
+++ backups/address.php
@@ -145,41 +145,38 @@ body{
 
 </style>
 <body>
+<div class="container">
+    <h2 class="mt-5">Enter Your Address</h2>
+    <form action="process_address.php" method="POST">
+        <!-- Add a hidden input field for user ID -->
+        <input type="hidden" name="user_id" value="1"> <!-- Assuming logged in user has ID 1. Modify this based on your login system. -->
+        <div class="form-group">
+            <label for="street">Street</label>
+            <input type="text" class="form-control" id="street" name="street" required>
+        </div>
+        <div class="form-group">
+            <label for="city">City</label>
+            <input type="text" class="form-control" id="city" name="city" required>
+        </div>
+        <div class="form-group">
+            <label for="state">State</label>
+            <input type="text" class="form-control" id="state" name="state">
+        </div>
+        <div class="form-group">
+            <label for="postal_code">Postal Code</label>
+            <input type="text" class="form-control" id="postal_code" name="postal_code" required>
+        </div>
+        <div class="form-group">
+            <label for="country">Country</label>
+            <input type="text" class="form-control" id="country" name="country" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <a href="../products/despay.php" class="btn btn-danger mr-3" style="border-color: black;">Cancel</a>
+    </form>
+</div>
 
-    <div class="container">
-        <h2 class="mt-5">Enter Your Address</h2>
-        <form action="process_address.php" method="POST">
-            <!-- Add a hidden input field for user ID -->
-            <input type="hidden" name="user_id" value="1"> <!-- Assuming logged in user has ID 1. Modify this based on your login system. -->
-            <div class="form-group">
-                <label for="street">Street</label>
-                <input type="text" class="form-control" id="street" name="street" required>
-            </div>
-            <div class="form-group">
-                <label for="city">City</label>
-                <input type="text" class="form-control" id="city" name="city" required>
-            </div>
-            <div class="form-group">
-                <label for="state">State</label>
-                <input type="text" class="form-control" id="state" name="state">
-            </div>
-            <div class="form-group">
-                <label for="postal_code">Postal Code</label>
-                <input type="text" class="form-control" id="postal_code" name="postal_code" required>
-            </div>
-            <div class="form-group">
-                <label for="country">Country</label>
-                <input type="text" class="form-control" id="country" name="country" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <button href="../products/despay.php" class="btn btn-danger mr-3" style="border-color: black;">Log-out</button>
-
-            
-        </form>
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
